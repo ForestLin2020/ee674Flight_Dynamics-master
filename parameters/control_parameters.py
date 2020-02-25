f33352612f43b98ada3ad445f940506e4ddfb1c6
ForestLin2020 <59840706+ForestLin2020@users.noreply.github.com>
2020-02-25 10:41:12 -0700

    Add files via upload

--- parameters/control_parameters.py
+++ parameters/control_parameters.py
@@ -5,7 +5,7 @@ import chap5.transfer_function_coef as TF
 import parameters.aerosonde_parameters as MAV
 
 gravity = MAV.gravity
-sigma = 0.05 # why??
+sigma = 0.05 # Which value should I put in ??
 Va0 = MAV.Va0
 
 #----------roll loop-------------
@@ -17,8 +17,8 @@ course_kp = 1000
 course_ki = 0.9955453022851157
 
 #----------sideslip loop-------------
-# sideslip_ki =
-# sideslip_kp =
+sideslip_ki = 0     # temperate value
+sideslip_kp = 1     # temperate value
 
 #----------yaw damper-------------
 # yaw_damper_tau_r =
@@ -32,10 +32,12 @@ K_theta_DC = 0.8555133079847909
 
 
 #----------altitude loop-------------
-altitude_kp = 40
-altitude_ki = 0.011213727829109888
-altitude_zone = 10 # just pick a number?
+altitude_kp = 2.5
+altitude_ki = 0.04485491131643955
+altitude_zone = 10  # equals Wh?
 
 #---------airspeed hold using throttle---------------
-airspeed_throttle_kp =
-airspeed_throttle_ki =
+
+
+airspeed_throttle_kp = 70
+airspeed_throttle_ki = 25.592629794342578
